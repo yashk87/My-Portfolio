175b202c41d8365d66ea615bc79751c98c4b4915
dev-3-atsit <dev3.atsit@gmail.com>
2024-08-26 21:24:24 +0530

    Prototype2 Completed

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -37,7 +37,7 @@ const Footer = () => {
           Copyright © 2024 Yash Khairkar
         </p>
 
-        <div className="flex items-center md:gap-3 gap-6">
+        <div className="flex items-center md:gap-3 gap-6 mt-5 md:mt-5">
           {socialMedia.map((info) => (
             <div
               key={info.id}
--- components/ui/MagicButton.tsx
+++ components/ui/MagicButton.tsx
@@ -18,7 +18,7 @@ const MagicButton = ({
       className=" inline-flex gap-2 h-12 animate-shimmer items-center justify-center rounded-2xl border-2 border-slate-800 bg-[linear-gradient(110deg,#000103,45%,#1e2631,55%,#000103)] bg-[length:200%_100%] px-10 font-medium text-white-100 transition-colors focus:outline-none "
     >
       <span className="text-[#6d56ff]">{position === "left" && icon}</span>
-      {title}
+      <span className="text-xs lg:text-base md:text-sm">{title}</span>
       <span className="text-[#6d56ff]">{position === "right" && icon}</span>
     </button>
   );
